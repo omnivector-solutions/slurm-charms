744985891605307a1930d0fc06d9d3bb306f4118
jamesbeedy <jamesbeedy@gmail.com>
2020-07-30 20:11:40 +0000

    revert to sending python with the charm

--- scripts/build_charms.sh
+++ scripts/build_charms.sh
@@ -2,8 +2,47 @@
 
 set -eux
 
+mkdir build/
+mkdir out/
+
+if ! [[ -f requirements.txt ]]; then
+    echo "Missing reuirements.txt"
+    exit -1
+fi
+
+docker run -it --rm \
+    -v `pwd`/requirements.txt:/srv/requirements.txt \
+    -v `pwd`/build:/srv/build \
+    quay.io/pypa/manylinux2014_x86_64 \
+    /bin/sh -c '/opt/python/cp38-cp38/bin/python -m venv --copies --clear /srv/build/venv && \
+                /opt/python/cp38-cp38/bin/pip install -r /srv/requirements.txt \
+                -t /srv/build/venv/lib/python3.8/site-packages && \
+                cp -r /usr/local/lib/libcrypt.* /srv/build/venv/lib/ && \
+                cp -r /opt/_internal/cpython-3.8.5/lib/python3.8 /srv/build/venv/lib/ && \
+                cp -r /opt/_internal/cpython-3.8.5/include/* /srv/build/venv/include/ && \
+                chown -R 1000:1000 /srv/build'
+
 
 for charm in slurmd slurmctld slurmdbd; do
     dir=charm-$charm
-    /snap/bin/charmcraft build --from $dir/
+    mkdir -p build/$dir/hooks
+    cp $dir/metadata.yaml build/$dir/
+    cp $dir/config.yaml build/$dir/
+    cp -r $dir/src build/$dir/
+    cp -r build/venv build/$dir/
+    cat <<EOF >build/$dir/dispatch
+#!/bin/sh
+PYTHONHOME=venv/ LD_LIBRARY_PATH="\${LD_LIBRARY_PATH}:venv/lib/" JUJU_DISPATCH_PATH="\${JUJU_DISPATCH_PATH:-\$0}" PYTHONPATH=lib:venv/lib/python3.8/site-packages ./venv/bin/python ./src/charm.py
+EOF
+    chmod +x build/$dir/dispatch
+
+    cd build/$dir/hooks/
+    ln -s ../dispatch install
+    ln -s ../dispatch start
+    ln -s ../dispatch upgrade-charm
+
+    cd ../ && \
+         zip --symlinks -r ../../out/$charm.charm * && \
+    cd ../../
+
 done
--- scripts/push_charms.sh
+++ scripts/push_charms.sh
@@ -7,5 +7,5 @@ stage=$1
 for charm in slurmctld slurmd slurmdbd; do
     s3="s3://omnivector-charms/$charm/$stage/"
     echo "Copying $charm.charm to $s3$charm.charm"
-    aws s3 cp $charm.charm $s3
+    aws s3 cp out/$charm.charm $s3
 done

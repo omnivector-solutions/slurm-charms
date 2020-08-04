eb595b87b97289016762b052520aec6247b760ea
jamesbeedy <jamesbeedy@gmail.com>
2020-08-04 19:47:23 +0000

    push charms to edge

--- scripts/push_charms.sh
+++ scripts/push_charms.sh
@@ -5,7 +5,7 @@ set -e
 stage=$1
 
 for charm in slurmctld slurmd slurmdbd; do
-    s3="s3://omnivector-charms/$charm/$stage/"
+    s3_loc="s3://omnivector-public-assets/charms/$charm/$stage/"
     echo "Copying $charm.charm to $s3$charm.charm"
-    aws s3 cp $charm.charm $s3
+    aws s3 cp $charm.charm $s3_loc --recursive --acl public-read
 done

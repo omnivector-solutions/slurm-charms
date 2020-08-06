1649b1dd5c5642cef554142b345c43b262fcb92c
jamesbeedy <jamesbeedy@gmail.com>
2020-08-06 20:35:58 +0000

    update grab logs to grab slurmrestd log too

--- scripts/grab_logs_from_slurm_units.sh
+++ scripts/grab_logs_from_slurm_units.sh
@@ -7,7 +7,7 @@ export PATH=$PATH:/snap/bin
 
 
 
-slurm_units=$(juju status slurmdbd slurmd slurmctld --format json | jq -r '.applications | map_values(.units) | .[] | keys | .[]')
+slurm_units=$(juju status slurmrestd slurmdbd slurmd slurmctld --format json | jq -r '.applications | map_values(.units) | .[] | keys | .[]')
 
 for slurm_unit in $slurm_units; do
     juju scp $slurm_unit:/var/log/juju/unit-*.log .

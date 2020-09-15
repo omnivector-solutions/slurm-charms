eb95defd730a6345a7863b93effe014b8e5a8ea1
Jesse <jesse1517@gmail.com>
2020-09-15 18:46:46 +0000

    include slurmrestd

--- scripts/pull_charms.sh
+++ scripts/pull_charms.sh
@@ -4,7 +4,7 @@ set -e
 
 stage=$1
 
-for charm in slurmctld slurmd slurmdbd; do
+for charm in slurmctld slurmd slurmdbd slurmrestd; do
     charm_loc="https://omnivector-public-assets.s3-us-west-2.amazonaws.com/charms/$charm/$stage/$charm.charm"
     echo "Copying $charm_loc to $charm.charm"
     wget $charm_loc

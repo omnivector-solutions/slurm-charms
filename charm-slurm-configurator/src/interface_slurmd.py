09dffb83d9c07d1ec52af769807ee38f60060bd3
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2021-04-06 14:23:00 -0300

    normalize shebang to usr/bin/env

--- charm-slurm-configurator/src/interface_slurmd.py
+++ charm-slurm-configurator/src/interface_slurmd.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Slurmd."""
 import copy
 import uuid
--- charm-slurmd/src/charm.py
+++ charm-slurmd/src/charm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """SlurmdCharm."""
 import copy
 import logging

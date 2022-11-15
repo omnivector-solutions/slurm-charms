8f24822a5f2e9917efdaf206702a1cd82e412227
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-15 16:08:48 +0000

    Adding tarfile member sanitization to extractall()

--- charm-slurmctld/src/etcd_ops.py
+++ charm-slurmctld/src/etcd_ops.py
@@ -46,7 +46,29 @@ class EtcdOps:
         with TemporaryDirectory(prefix="omni") as tmp_dir:
             logger.debug(f"## extracting {resource_path} to {tmp_dir}")
             with tarfile.open(resource_path, 'r') as tar:
-                tar.extractall(path=tmp_dir)
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=tmp_dir)
 
             usrbin = Path("/usr/bin")
             logger.debug(f"## installing etcd in {usrbin}")
